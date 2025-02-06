bcdc6dece311cf78953111b857291c8a7ba5b5bc
Hannah Braswell <hbraswel@redhat.com>
2025-02-06 12:34:07 -0500

    fix: updating standard access for adapter

--- pkg/log/wrap_log.go
+++ pkg/log/wrap_log.go
@@ -2,9 +2,10 @@ package log
 
 import (
 	"fmt"
-	"github.com/charmbracelet/log"
+	charmlog "github.com/charmbracelet/log"
 	"github.com/hashicorp/go-hclog"
-	"
+	"io"
+	"os"
 )
 
 var ErrMissingValue = fmt.Errorf("missing value")
@@ -12,11 +13,11 @@ var ErrMissingValue = fmt.Errorf("missing value")
 // Need to initialize function
 
 // Wrap the function
-func Wrap(log *log.StandardLogOptions) hclog.Logger { return &CharmHclog{log} }
+func WrapLog(charmlog *charmlog.StandardLogOptions) hclog.Logger { return &CharmHclog{charmlog} }
 
 // CharmHclog will be a structure that accesses the attributes of charm log
 type CharmHclog struct {
-	logger *log.Logger
+	logger *charmlog.Logger
 }
 
 // LoggerOption is an option for a logger (from charm logger)
@@ -25,44 +26,43 @@ type CharmHclog struct {
 // CharmHclog will implement the hclog.Logger
 var _ hclog.Logger = &CharmHclog{}
 
-var hclogCharmLevels = map[hclog.Level]log.Level{
-	hclog.NoLevel: log.InfoLevel,  // There is no "NoLevel" equivalent in charm, use info
-	hclog.Trace:   log.DebugLevel, // There is no "Trace" equivalent in charm, use debug
-	hclog.Debug:   log.DebugLevel,
-	hclog.Info:    log.InfoLevel,
-	hclog.Warn:    log.WarnLevel,
-	hclog.Error:   log.ErrorLevel,
-	hclog.Off:     log.FatalLevel, // There is no "Off" level equivalent in charm
+var hclogCharmLevels = map[hclog.Level]charmlog.Level{
+	hclog.NoLevel: charmlog.InfoLevel,  // There is no "NoLevel" equivalent in charm, use info
+	hclog.Trace:   charmlog.DebugLevel, // There is no "Trace" equivalent in charm, use debug
+	hclog.Debug:   charmlog.DebugLevel,
+	hclog.Info:    charmlog.InfoLevel,
+	hclog.Warn:    charmlog.WarnLevel,
+	hclog.Error:   charmlog.ErrorLevel,
+	hclog.Off:     charmlog.FatalLevel, // There is no "Off" level equivalent in charm
 }
 
-var charmHclogLevels = map[log.Level]hclog.Level{
-	log.DebugLevel: hclog.Debug,
-	log.InfoLevel:  hclog.Info,
-	log.WarnLevel:  hclog.Warn,
-	log.ErrorLevel: hclog.Error,
-	log.FatalLevel: hclog.Error, // There is no "fatal" equivalent in hclog
+var charmHclogLevels = map[charmlog.Level]hclog.Level{
+	charmlog.DebugLevel: hclog.Debug,
+	charmlog.InfoLevel:  hclog.Info,
+	charmlog.WarnLevel:  hclog.Warn,
+	charmlog.ErrorLevel: hclog.Error,
+	charmlog.FatalLevel: hclog.Error, // There is no "fatal" equivalent in hclog
 }
 
 func (c *CharmHclog) Log(level hclog.Level, msg string, args ...interface{}) {
-	c.logger.Logf(hclogCharmLevels[level], fmt.Sprintf(msg, args...))
+	c.logger.Log(hclogCharmLevels[level], fmt.Sprintf(msg, args...))
 }
 func (c *CharmHclog) Trace(msg string, args ...interface{}) {
-	c.logger.Debugf(msg, args...)
+	c.logger.Debug(msg, args...)
 }
 func (c *CharmHclog) Debug(msg string, args ...interface{}) {
-	c.logger.Debugf(msg, args...)
+	c.logger.Debug(msg, args...)
 }
 func (c *CharmHclog) Info(msg string, args ...interface{}) {
-	c.logger.Infof(msg, args...)
+	c.logger.Info(msg, args...)
 }
 func (c *CharmHclog) Warn(msg string, args ...interface{}) {
-	c.logger.Warnf(msg, args...)
+	c.logger.Warn(msg, args...)
 }
 func (c *CharmHclog) Error(msg string, args ...interface{}) {
-	c.logger.Errorf(msg, args...)
+	c.logger.Error(msg, args...)
 }
 
-// Functions from go-hc-lo
 // Functions from go-hc-log
 
 func (c *CharmHclog) IsTrace() bool     { return false }
@@ -86,24 +86,30 @@ func (c *CharmHclog) Named(name string) hclog.Logger {
 // go-hclog logger resetnamed function to implement
 
 //func (c *CharmHclog) ResetNamed(name string) hclog.Logger {
-//	logger, err := log.NewWithOptions()
+//	logger, err := charmlog.NewWithOptions()
 //	if err != nil { panic(err) }
 //	return &CharmHclog{logger.WithAttrs(name)}}
 //	return c.ResetNamed(name) }
 
 // Enables setting log level
 func (c *CharmHclog) SetLevel(level hclog.Level) {
-	log.SetLevel(hclogCharmLevels[level])
+	charmlog.SetLevel(hclogCharmLevels[level])
 }
 
-func (c *CharmHclog) GetLevel() hclog.Level { return charmHclogLevels[log.Level(hclog.Level(level))] }
+// GetLevel using charm logger GetLevel
+func (c *CharmHclog) GetLevel() hclog.Level {
+	return charmHclogLevels[hclog.Level(charmlog.GetLevel())]
+}
+
+// GetLevel using charm logger Level method to extract
+//func (c *CharmHclog) GetLevel() hclog.Level { return charmHclogLevels[hclog.Level(charmlog.Level(level))]}
 
 // Look at stdlog.go for forcing level
 // The standard logger needs to be implemented to return a standard logger of the charm type
-func (c *CharmHclog) StandardLogger(opts *hclog.StandardLoggerOptions) *log.Logger {
-	return log.NewWithOptions(c.logger.StandardLog())
+func (c *CharmHclog) StandardLogger(opts *hclog.StandardLoggerOptions) *charmlog.Logger {
+	return charmlog.NewWithOptions(c.logger.StandardLog())
 }
 
 func (c *CharmHclog) StandardWriter(opts *hclog.StandardLoggerOptions) io.Writer { return os.Stdout }
 
-func Logger() *log.Logger { return log.StandardLog() }
+func Logger() *charmlog.Logger { return charmlog.StandardLog() }
