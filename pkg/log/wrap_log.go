4782308ad89f38c6ddce80ac4fe0831b787fa527
Hannah Braswell <hbraswel@redhat.com>
2025-02-10 09:42:51 -0500

    feat: updating comments for review hints

--- pkg/log/wrap_log.go
+++ pkg/log/wrap_log.go
@@ -15,10 +15,10 @@ func init() {
 	charmlog.Default()
 }
 
-// Wrap the function
+// Wrap the functionality of charmlogger in hclogger
 func WrapLog(charmlog *charmlog.Logger) hclog.Logger { return &CharmHclog{charmlog} }
 
-// CharmHclog will be a structure that accesses the attributes of charm log
+// CharmHclog will be a structure that accesses the attributes of charm logger
 type CharmHclog struct {
 	logger *charmlog.Logger
 }
@@ -27,6 +27,8 @@ type CharmHclog struct {
 
 var _ hclog.Logger = &CharmHclog{}
 
+// Declaring hclogCharmLevels as a map with key values for adapting hclog to charmlog
+// key: value -> hclog.LEVEL: charmlog.LEVEL
 var hclogCharmLevels = map[hclog.Level]charmlog.Level{
 	hclog.NoLevel: charmlog.InfoLevel,  // There is no "NoLevel" equivalent in charm, use info
 	hclog.Trace:   charmlog.DebugLevel, // There is no "Trace" equivalent in charm, use debug
@@ -37,6 +39,7 @@ var hclogCharmLevels = map[hclog.Level]charmlog.Level{
 	hclog.Off:     charmlog.FatalLevel, // There is no "Off" level equivalent in charm
 }
 
+// Declaring charmHclogLevels to map the key: value pairs as charmlogger and hclog
 var charmHclogLevels = map[charmlog.Level]hclog.Level{
 	charmlog.DebugLevel: hclog.Debug,
 	charmlog.InfoLevel:  hclog.Info,
@@ -44,6 +47,10 @@ var charmHclogLevels = map[charmlog.Level]hclog.Level{
 	charmlog.ErrorLevel: hclog.Error,
 	charmlog.FatalLevel: hclog.Error, // There is no "fatal" equivalent in hclog
 }
+
+// c will have information from the CharmHclog structure and will access the charm logger
+// Log will use the level of hclog and do the identical logger operation using the charmlogger
+// The map defines the level matches
 
 func (c *CharmHclog) Log(level hclog.Level, msg string, args ...interface{}) {
 	c.logger.Log(hclogCharmLevels[level], fmt.Sprintf(msg, args...))
@@ -78,6 +85,8 @@ func (c *CharmHclog) With(args ...interface{}) hclog.Logger {
 // Need to configure a Name function
 func (c *CharmHclog) Name() string { return c.Name() }
 
+//func (c *CharmHclog) Name() string { return hclog.Logger(c.logger).Name() }
+
 // Take input and then prepend name string
 func (c *CharmHclog) Named(name string) hclog.Logger {
 	return &CharmHclog{c.logger.With()}
@@ -86,6 +95,10 @@ func (c *CharmHclog) Named(name string) hclog.Logger {
 // go-hclog logger resetnamed function to implement
 func (c *CharmHclog) ResetNamed(name string) hclog.Logger {
 	c.Named(name)
+	logger, err := charmlog.SetOutput()
+	if err != nil {
+		panic(err)
+	}
 	return &CharmHclog{logger: charmlog.New()}
 }
 
